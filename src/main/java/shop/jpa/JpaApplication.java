ed0c3892ab9b015a3437f38ccd9c275ec14360ae
김태목 <using1524@naver.com>
2020-02-10 02:00:43 +0900

    외부 yml 경로 추가 & actuator 의존성 추가
    무중단 배포 - Nginx 활용을 위해 외부 yml을 이용, 어플리케이션 상태 확인을 위한 actuator 의존성 추가

--- src/main/java/shop/jpa/JpaApplication.java
+++ src/main/java/shop/jpa/JpaApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/springboot-webservice/real-application.yml";
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
 	}
