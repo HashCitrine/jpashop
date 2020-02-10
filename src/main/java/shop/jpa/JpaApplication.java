15a9efc082924168b0a1c360631d8e95e45e9297
김태목 <using1524@naver.com>
2020-02-10 16:26:15 +0900

    yml 경로 오타 수정
    서버 업로드 시 불러올 외부 yml 경로 수정

--- src/main/java/shop/jpa/JpaApplication.java
+++ src/main/java/shop/jpa/JpaApplication.java
@@ -8,7 +8,7 @@ public class JpaApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "/app/config/springboot-webservice/real-application.yml";
+			+ "/app/config/jpashop/real-application.yml";
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
